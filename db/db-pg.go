25ca57e08654e499649b3a5fb1289345a3ef65df
Santosh Suresh <santosh.suresh@jifflenow.com>
2015-03-01 21:54:59 +0530

    Added the db files

---
+++ db/db-pg.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"time"
+)
+
+const (
+	DB_USER     = "santoshsuresh"
+	DB_PASSWORD = "aa"
+	DB_NAME     = "bluesky"
+)
+
+func main() {
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
+		DB_USER, DB_PASSWORD, DB_NAME)
+	db, err := sql.Open("postgres", dbinfo)
+	checkErr(err)
+	defer db.Close()
+
+	rows, err := db.Query("select * from userinfo")
+	checkErr(err)
+	for rows.Next() {
+		var uid int
+		var username string
+		var department string
+		var created time.Time
+		err = rows.Scan(&uid, &username, &department, &created)
+		checkErr(err)
+		fmt.Println("uid | username | department | created ")
+		fmt.Printf("%3v | %8v | %6v | %6v\n", uid, username, department, created)
+	}
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
